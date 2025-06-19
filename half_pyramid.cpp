ca03d6864da61d62dbf1708500da0a0735a9f31d
waseans <115150143+waseans@users.noreply.github.com>
2025-06-19 11:35:51 +0530

    added new

--- half_pyramid.cpp
+++ half_pyramid.cpp
@@ -1,15 +1,13 @@
-
-//printing rectangle
-
 #include <iostream>
 using namespace std;
 
 int main() {
 
+int n=5;
 
-for (int i=0;i<4;i++) {
-    for (int j=0;j<5;j++) {
-        cout << "*" ;
+for(int i=1;i<=1;i++) {
+    for (int j=1;j<=i;j++){
+            cout << "*";
     }
     cout << endl;
 }
